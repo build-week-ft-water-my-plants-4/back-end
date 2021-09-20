823e947a7c78c934d8498f02aec0cba2f78c9917
Leu Family <romanianfobjr@gmail.com>
2021-09-20 13:30:08 -0500

    database created

---
+++ data/migrations/20210920175543_create-tables.js
@@ -0,0 +1,36 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('users', tbl => {
+            tbl.increments('user_id')
+            tbl.string('username')
+                .unique()
+                .notNullable()
+            tbl.integer('phone_number')
+                .notNullable()
+            tbl.string('password')
+                .notNullable()
+        })
+        .createTable('plants', tbl => {
+            tbl.increments('plant_id')
+            tbl.string('nickname')
+                .notNullable()
+            tbl.string('species')
+                .notNullable()
+            tbl.string('h2o_frequency')
+                .notNullable()
+            tbl.text('image_url')
+            tbl.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('user_id').inTable('users')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE')
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('plants')
+        .dropTableIfExists('users')
+};
--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
         client: 'sqlite3',
         useNullAsDefault: true,
         connection: {
-            filename: './data/plants',
+            filename: './data/plants.db3',
         },
         migrations: {
             directory: './data/migrations',
