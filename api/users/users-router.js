0a3d1173b39f7635e6327a092d86eb77cbf8cf2d
Leu Family <romanianfobjr@gmail.com>
2021-09-20 15:22:49 -0500

    add user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -9,4 +9,21 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
+router.get('/:id', (req, res, next) => {
+    const { id } = req.params
+    Users.findUserById(id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Users.addUser(req.body)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(next)
+})
+
 module.exports = router
